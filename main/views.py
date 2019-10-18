73101e7b56a3524341961c6581bf6085d7df030a
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2019-10-17 22:59:26 -0700

    adding bootstrap and using template tags

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+from django.template import loader
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Todo
+
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,    
+        'today': datetime.now(),
+        'app_name': "Super Todos"
+    }
+    return render(request, 'main/index.html', context=context)
+    #template = loader.get_template('main/index.html')
+    #return HttpResponse(template.render(context, request))
