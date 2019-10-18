82c47577ac28a67f3d632c459fd36f2c8f30302e
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2019-10-18 00:41:04 -0700

    Adding details page, using template inheritance

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, todo_detail
 
 urlpatterns = [
     path('index', index),
+    path('todo/<int:todo_id>', todo_detail),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from django.template import loader
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from .models import Todo
 
@@ -15,3 +15,19 @@ def index(request):
     return render(request, 'main/index.html', context=context)
     #template = loader.get_template('main/index.html')
     #return HttpResponse(template.render(context, request))
+
+def todo_detail(request, todo_id):
+  todo = get_object_or_404(Todo, pk=todo_id)
+
+  context = {
+    'todo': todo
+  }
+  return render(request, 'main/todo_detail.html', context=context)
+  # try:
+  #   todo = Todo.objects.get(pk=todo_id)
+  #   context = {
+  #     'todo': todo
+  #   }
+  #   return render(request, 'main/todo_detail.html', context=context)
+  # except Todo.DoesNotExist:
+  #   raise Http404("Todo with id: {} does not exist.".format(todo_id))
