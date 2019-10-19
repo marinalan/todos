6115db73d17f34b9ee6ded62cbcf5dcf6e07b705
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2019-10-19 00:11:44 -0700

    using django-crispy-forms

--- todos/settings.py
+++ todos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, "static"),
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
