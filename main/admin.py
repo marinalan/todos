40cfaa00435893e891800f1552f88afd5eeee564
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2019-11-25 18:30:35 -0800

    Todo model changes

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'due_date', 'done')
-    list_filter = ('done', 'due_date')
+    list_display = ('title', 'due_date', 'state')
+    list_filter = ('state', 'due_date')
     ordering = ('-due_date', )
 
 admin.site.register(Todo, TodoAdmin)
--- main/models.py
+++ main/models.py
@@ -2,10 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=255)
-    due_date = models.DateField()
-    done = models.BooleanField(default=False)
+    TODO = 'todo'
+    IN_PROGRESS = 'in-progress'
+    DONE = 'done'
+    STATE_CHOICES = [
+        (TODO, 'To Do'),
+        (IN_PROGRESS, 'In Progress'),
+        (DONE, 'Done')
+    ]
     description = models.TextField()
+    due_date = models.DateField()
+    state = models.CharField(
+        max_length=11,
+        choices=STATE_CHOICES,
+        default=TODO
+    )
+
+    def _title(self):
+        nl = self.description.find("\n")
+        lb = min(120, nl) if nl > -1 else 120
+        return self.description[:lb]
+
+    title = property(_title)
 
     def __str__(self):
         return self.title
